dcb7e6299896e28ddb147fb4b862e2ce80270df6
Maxim <matiush2018@yandex.ru>
2024-11-17 13:48:18 +0300

    chore: added redux

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,17 @@
 import {
     RouterProvider,
 } from "react-router-dom";
+import {Provider as ReduxProvider} from 'react-redux'
 
 import {router} from "pages";
+import {store} from "./store";
 
 const App = () => {
-    return <RouterProvider router={router}/>
+    return (
+        <ReduxProvider store={store}>
+            <RouterProvider router={router}/>
+        </ReduxProvider>
+    )
 };
 
 export default App
---
+++ src/app/store/index.ts
@@ -0,0 +1,8 @@
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
+
+const mainReducer = combineReducers({
+});
+
+export const store = configureStore({
+    reducer: mainReducer,
+})
